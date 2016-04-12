68e0871c7cc6d0e8cfc0368226465a090aab4528
yuandondon <yuan316@gmail.com>
2016-04-12 04:06:21 -0400

    add gridView and urlImageToBitmap function in rofileActivity.java

--- app/src/main/java/com/photodrop/photodrop/ProfileActivity.java
+++ app/src/main/java/com/photodrop/photodrop/ProfileActivity.java
@@ -1,6 +1,8 @@
 package com.photodrop.photodrop;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,7 +14,14 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import android.widget.GridView;
+
+import java.net.URL;
+import java.util.ArrayList;
+
 public class ProfileActivity extends AppCompatActivity {
+    private GridView imageGrid;
+    private ArrayList<Bitmap> bitmapList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +32,33 @@ public class ProfileActivity extends AppCompatActivity {
 
         // Adds the back button to the toolbar since we're adding it dynamically
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
 
+
+        this.imageGrid = (GridView) findViewById(R.id.profileActivityGridview);
+        this.bitmapList = new ArrayList<Bitmap>();
+
+        try {
+            System.out.print(">_<!\n\n");
+            for(int i = 0; i < 10; i++) {
+                this.bitmapList.add(urlImageToBitmap("http://placehold.it/150x150"));
+                System.out.print(">_<!!\n\n");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        this.imageGrid.setAdapter(new ImageAdapter(this, this.bitmapList));
+
+    }
+    private Bitmap urlImageToBitmap(String imageUrl) throws Exception {
+        Bitmap result = null;
+        URL url = new URL(imageUrl);
+        if(url != null) {
+            result = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+            //result = BitmapFactory.decodeStream();
+        }
+        return result;
+    }
     /**
      * Associates the menu_profile with this activity's menu
      */
