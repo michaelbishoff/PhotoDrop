ca27f9aaf8bde4aae5a32ad41f4add8a5d88add7
yuandondon <yuan316@gmail.com>
2016-04-23 16:31:06 -0400

    add app/src/main/java/com/photodrop/photodrop/ImageAdapter.java

---
+++ app/src/main/java/com/photodrop/photodrop/ImageAdapter.java
@@ -0,0 +1,50 @@
+package com.photodrop.photodrop;
+
+/**
+ * Created by yuan316 on 4/12/16.
+ */
+import android.app.*;
+import android.os.*;
+import android.widget.*;
+import java.util.*;
+import android.graphics.*;
+import android.view.*;
+import android.content.*;
+
+public class ImageAdapter extends BaseAdapter {
+
+    private Context context;
+    private ArrayList<Bitmap> bitmapList;
+
+    public ImageAdapter(Context context, ArrayList<Bitmap> bitmapList) {
+        this.context = context;
+        this.bitmapList = bitmapList;
+    }
+
+    public int getCount() {
+        return this.bitmapList.size();
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {
+            imageView = new ImageView(this.context);
+            imageView.setLayoutParams(new GridView.LayoutParams(115, 115));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        imageView.setImageBitmap(this.bitmapList.get(position));
+        return imageView;
+    }
+
+}
