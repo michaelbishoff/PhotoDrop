7e8949fae9b303b2e7b82ece54225d428487e6b6
Michael Bishoff <bishoff1@umbc.edu>
2016-05-08 15:54:56 -0400

    Centered login and changed app logo

--- app/src/main/java/com/photodrop/photodrop/LoginActivity.java
+++ app/src/main/java/com/photodrop/photodrop/LoginActivity.java
@@ -3,7 +3,6 @@ package com.photodrop.photodrop;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
